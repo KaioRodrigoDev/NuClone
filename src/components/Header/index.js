a7fb8b5059cb5b06632321eec09b74daf614dfc6
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-06-25 20:55:36 -0300

    Finalização do projeto

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native'
 
 import { Feather } from '@expo/vector-icons'
+import { MotiView, MotiText } from 'moti'
 
 const statusBarHeight = StatusBar.currentHeight
   ? StatusBar.currentHeight + 22
@@ -16,13 +17,43 @@ const statusBarHeight = StatusBar.currentHeight
 export default function Header({ name }) {
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.username}> {name}</Text>
+      <MotiView
+        style={styles.content}
+        from={{
+          translateY: -150,
+          opacity: 0
+        }}
+        animate={{
+          translateY: 0,
+          opacity: 1
+        }}
+        transition={{
+          type: 'timing',
+          duration: 700,
+          delay: 300
+        }}
+      >
+        <MotiText
+          style={styles.username}
+          from={{
+            translateX: -300
+          }}
+          animate={{
+            translateX: 0
+          }}
+          transition={{
+            type: 'timing',
+            duration: 700,
+            delay: 600
+          }}
+        >
+          {name}
+        </MotiText>
 
         <TouchableOpacity style={styles.buttonUser} activeOpacity={0.9}>
           <Feather name="user" size={24} color="#fff" />
         </TouchableOpacity>
-      </View>
+      </MotiView>
     </View>
   )
 }
